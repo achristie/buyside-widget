e1fcdb1d6d1d2a84bc8748f1b27fb1f4a4239374
achristie <awchristie86@gmail.com>
2015-01-30 10:50:18 -0500

    masonry

--- public/javascripts/controllers/searchres.js
+++ public/javascripts/controllers/searchres.js
@@ -1,6 +1,10 @@
 bsApp.controller('SearchResCtrl', function ($scope, $location, $filter, globalsearch) {
 	$scope.q = $location.search().q;
 	$scope.entities = [];
+	$scope.activeEntity = {
+		index: 0,
+		type: ''
+	};
 
 	$scope.$watch('q', function (n, o) {
 		globalsearch.getEntities(n).then(function (d) {
@@ -10,5 +14,9 @@ bsApp.controller('SearchResCtrl', function ($scope, $location, $filter, globalse
 
 	$scope.min = function(arr) {
 		return $filter('min')($filter('map')(arr, 'id'));
+	}
+
+	$scope.toggleActive = function (e, $index) {
+		$scope.activeEntity = { type: e.type, index: $index };
 	}
 });
